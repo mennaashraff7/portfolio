11dc8a57d009a554df815afb8e07dce0a6be65e2
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2020-11-07 22:59:20 +0200

    3rd commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const aboutUs = document.querySelector(`.about-us`);
 const skills = document.querySelector(`.skills-section`);
 const bars = document.querySelector(`.bars`);
 const portfolio = document.querySelector(`.images`);
+const menu = document.querySelector(`.sideBar`);
+const menuBtn = document.querySelector(`aside button`);
 
 function onScroll(){
    var aboutUsPosition = aboutUs.getBoundingClientRect().top;
@@ -31,5 +33,9 @@ function skillsAppear(){
 function portfolioAppear(){
     portfolio.classList.add('appear-img');
 }
+function showMenu(){
+    menu.classList.toggle('display');
+}
 
+menuBtn.addEventListener('click',showMenu);
 window.addEventListener('scroll', onScroll);
