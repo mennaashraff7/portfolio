db9bf1be6b61f8a60dcac700bc714209fb11c83b
mennaashraff7 <39194503+mennaashraff7@users.noreply.github.com>
2020-11-09 18:13:07 +0200

    4th commit

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const bars = document.querySelector(`.bars`);
 const portfolio = document.querySelector(`.images`);
 const menu = document.querySelector(`.sideBar`);
 const menuBtn = document.querySelector(`aside button`);
+const nav = document.querySelector(`.nav`);
+const logo = document.querySelector(`.logo`);
+const content = document.querySelector(`.content`);
+const menuElements = document.querySelectorAll(`.menu div a`);
+
 
 function onScroll(){
    var aboutUsPosition = aboutUs.getBoundingClientRect().top;
@@ -35,7 +40,27 @@ function portfolioAppear(){
 }
 function showMenu(){
     menu.classList.toggle('display');
+    
+}
+function changeNavColor(){
+    nav.style.background = "white";
+    nav.style.transition = "0.9s";
+    nav.style.boxShadow=" 0px 0px 10px 0 grey";
+    logo.style.color ="#001433";
+    menuElements.forEach(element=>element.style.color="brown");
+    
+   var navPosition = nav.getBoundingClientRect().top;
+   var contentPosition = content.getBoundingClientRect().top;
+    
+    if(navPosition===contentPosition){
+        nav.style.background = "transparent";
+        nav.style.transition = "0.9s";
+        logo.style.color ="white";
+        nav.style.boxShadow="none";
+       menuElements.forEach(element=>element.style.color="white");
+}
 }
 
 menuBtn.addEventListener('click',showMenu);
 window.addEventListener('scroll', onScroll);
+window.addEventListener('scroll', changeNavColor);
